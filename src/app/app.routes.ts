d8998329449a8d3de0ac72b5166245e47fb8d002
Joao Pedro <jpcsnoberto@gmail.com>
2024-12-04 20:16:06 -0300

    feat: header component with routing; update styles and assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './views/feed/feed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'feed', component: FeedComponent },
+  { path: '', redirectTo: '/feed', pathMatch: 'full' },
+];
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  onRouterLinkActive(routerLink: any) {
+    console.log('routerLink', routerLink);
+  }
+}
