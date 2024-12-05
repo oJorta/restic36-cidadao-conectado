7573f6884fd8626781523dd82c08204379f55f09
Joao Pedro <jpcsnoberto@gmail.com>
2024-12-05 20:58:01 -0300

    feat: enhance layout and styling of feed and consult components; add category selection functionality

--- src/app/views/consult/consult.component.ts
+++ src/app/views/consult/consult.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { PageHeaderComponent } from "../../components/page-header/page-header.component";
 
 @Component({
   selector: 'app-consult',
   standalone: true,
-  imports: [],
+  imports: [PageHeaderComponent],
   templateUrl: './consult.component.html',
   styleUrl: './consult.component.css'
 })
 export class ConsultComponent {
+  selectedCategory: string = 'renuncias';
 
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
 }
