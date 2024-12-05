8d6123057a055190ac5593be540627e99ec075d2
Joao Pedro <jpcsnoberto@gmail.com>
2024-12-04 23:34:17 -0300

    feat: add new post and consult components; update routing to include new paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { FeedComponent } from './views/feed/feed.component';
+import { NewPostComponent } from './views/new-post/new-post.component';
+import { ConsultComponent } from './views/consult/consult.component';
 
 export const routes: Routes = [
   { path: 'feed', component: FeedComponent },
+  { path: 'novo-topico', component: NewPostComponent },
+  { path: 'consultar', component: ConsultComponent },
   { path: '', redirectTo: '/feed', pathMatch: 'full' },
 ];
