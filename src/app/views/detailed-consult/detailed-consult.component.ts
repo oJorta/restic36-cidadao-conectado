00696f1183039122162ad62e7d5379a0715e99cb
Joao Pedro <jpcsnoberto@gmail.com>
2024-12-06 17:56:01 -0300

    feat: implement dynamic data binding in detailed consult component; enhance styling and data source information

--- src/app/views/detailed-consult/detailed-consult.component.ts
+++ src/app/views/detailed-consult/detailed-consult.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
+import { CurrencyPipe } from '@angular/common';
 import { PageHeaderComponent } from "../../components/page-header/page-header.component";
+import { Resign } from '../../types/models';
+import { ResignService } from '../../services/resign/resign.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detailed-consult',
   standalone: true,
-  imports: [PageHeaderComponent],
+  imports: [PageHeaderComponent, CurrencyPipe],
   templateUrl: './detailed-consult.component.html',
   styleUrl: './detailed-consult.component.css'
 })
 export class DetailedConsultComponent {
+  resign!: Resign;
+  resignId!: number;
+  constructor(
+    private resignService: ResignService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.resignService.getResigns().subscribe(data => {
+      this.resignId = this.route.snapshot.params['id'];
+      this.resign = data[this.resignId];
+      console.log('Resign:', this.resign);
+    });
+  }
+
+
 
 }
