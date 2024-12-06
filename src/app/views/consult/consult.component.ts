8c1410dff4482f6ed28a687df46e6b7864383469
Joao Pedro <jpcsnoberto@gmail.com>
2024-12-06 13:37:00 -0300

    feat: refactor consult component to use dynamic data binding; implement navigation to detailed consult view

--- src/app/views/consult/consult.component.ts
+++ src/app/views/consult/consult.component.ts
@@ -1,28 +1,38 @@
 import { Component } from '@angular/core';
 import { PageHeaderComponent } from "../../components/page-header/page-header.component";
 import { ResignService } from '../../services/resign/resign.service';
+import { Resign } from '../../types/models';
+import { CurrencyPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consult',
   standalone: true,
-  imports: [PageHeaderComponent],
+  imports: [PageHeaderComponent, CurrencyPipe],
   templateUrl: './consult.component.html',
   styleUrl: './consult.component.css'
 })
 export class ConsultComponent {
   selectedCategory: string = 'renuncias';
+  data: Resign[] = [];
 
   constructor(
-    private resignService: ResignService
+    private resignService: ResignService,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.resignService.getResigns().subscribe(resigns => {
-      console.log(resigns);
+      this.data = resigns;
     });
   }
 
   selectCategory(category: string) {
     this.selectedCategory = category;
+  }
+
+  navigateToDetailedConsult(resignId: number) {
+    console.log('Navigating to detailed consult for resignId:', resignId);
+    this.router.navigate([`/consultar/${resignId}`]);
   }
 }
